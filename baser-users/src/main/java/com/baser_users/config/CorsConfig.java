d60bb9a7b8562b689782057029dc3b18cfa5d631
DESKTOP-LL05JTU\David Stefani Prado <daviprado1995@gmail.com>
2026-04-27 08:23:01 -0300

    Add configuração de cors para liberar acesso

--- baser-users/src/main/java/com/baser_users/config/CorsConfig.java
+++ baser-users/src/main/java/com/baser_users/config/CorsConfig.java
@@ -14,14 +14,15 @@ public class CorsConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200",
+                "http://127.0.0.1:4200",
+                "http://192.168.79.*:4200",
+                "http://10.66.152.*:4200"));
 
-        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT","PATCH","DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "Accept"));
 
         configuration.setAllowCredentials(true);
-
         configuration.setMaxAge(3600L);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
